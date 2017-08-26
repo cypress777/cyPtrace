4d63e592f2c96e6599bd95ed5b96f1a6788e8599
cypress777 <chenypress@gmail.com>
2017-08-26 10:43:22 +0800

    start cyPtrace program

---
+++ cyPtrace.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/reg.h>
+#include<sys/time.h>
+#include<sys/ptrace.h>
+#include<./util.h>
+#include<./elf.h>
+
+
